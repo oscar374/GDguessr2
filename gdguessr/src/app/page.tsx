2f5d5a4a96c340448f81407ed6c442178110a1c5
oscar374 <77411330+oscar374@users.noreply.github.com>
2025-10-03 10:23:23 +0200

    init

---
+++ gdguessr/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1 className="text-3xl font-bold underline">GDguessr frontend</h1>
+    </div>
+  );
+}
