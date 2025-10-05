33ab5c999abc5e4c8824dc22da34a1ac373c294d
oscar374 <77411330+oscar374@users.noreply.github.com>
2025-10-05 08:35:38 +0200

    pointercrate endpoint connection

--- backend/api.py
+++ backend/api.py
@@ -1,3 +1,15 @@
-import fastapi
+from fastapi import FastAPI 
+import random
+import requests
 
-a = 2
+app = FastAPI() 
+
+@app.get("/question/{min}/{max}")
+async def get_question(min: int, max: int):
+    placement = random.randint(min, max)
+    pointerCrateResponse = requests.get(f"https://pointercrate.com/api/v2/demons/listed")
+    pointerCrateJSON = pointerCrateResponse.json();
+    levelName = next((item["name"] for item in pointerCrateJSON if item["position"] == placement), None)
+    
+    return {"placement": placement, "levelName": levelName}
+    
--- gdguessr/src/app/game/page.tsx
+++ gdguessr/src/app/game/page.tsx
@@ -1,7 +1,9 @@
+import NavBar from "../components/navbar";
 
 export default function Game() {
   return (
     <div className="">
+      <NavBar />  
       <h1 className="">Game page</h1>
     </div>
   );
